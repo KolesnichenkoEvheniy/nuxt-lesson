44555cfa4071d772bea678692910b9af00fb0298
Evgeniy <evgeniy.web@ukr.net>
2017-06-03 14:24:28 +0300

    some changes

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,7 @@
         Universal Vue.js Application
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <nuxt-link to="/about">About page</nuxt-link>
       </div>
     </div>
   </section>
